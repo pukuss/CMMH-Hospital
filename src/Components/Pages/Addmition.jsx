345361a964c453185580ad16d11e1fa51826fab4
prashanta mondal <prashantaa@gmail.com>
2025-12-08 15:48:49 +0530

    0.0.0 p-0.03 added some funtion

--- src/Components/Pages/Addmition.jsx
+++ src/Components/Pages/Addmition.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Addmition() {
   setTimeout(() => {
@@ -16,6 +17,28 @@ function Addmition() {
       >
          <div className=" h-full w-full flex items-center justify-center">
           {/* Your content here */}
+          
+        </div>
+
+        <div className="max-w-[1800px] m-auto rounded-2xl mt-10">
+          <img
+           className=" m-auto rounded-2xl"
+          src="/src/assets/images/Addmition_bg.png " alt="" srcset="" />
+
+          <div className=" p-5">
+            <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  ` ${
+                    isActive
+                      ? "text-emerald-500 border border-emerald-700 italic"
+                      :  " hover:bg-amber-50 hover:text-red-600 text-shadow-2xs text-shadow-fuchsia-500 transition-all delay-150  text-amber-50 font-knews bg-red-800 text-2xl md:text-3xl px-25 md:px-55 rounded-2xl py-2"
+                  }`
+                }>
+                <span> ENROL NOW </span> 
+              </NavLink>
+             
+          </div>
         </div>
       </div>
     </>
