fb210b7482f7afdffedcb555c6e358c072432c44
prashanta mondal <prashantaa@gmail.com>
2025-12-30 09:30:43 +0530

    0.0.3.3v ---fix but

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -75,7 +75,7 @@ function Home() {
                   //   backgroundClip: "text",
                   //   color: "transparent",
                   // }}
-                  className=" not-md:text-[25px] text-6xl font-bold font-BBH">
+                  className=" not-md:text-[5vw] text-6xl font-bold font-BBH">
                   CarePlus Multispeciality Medicale College & Hospital
                 </span>
 
@@ -115,14 +115,14 @@ function Home() {
                 <span>{/* <Logo /> */}</span>
 
                 {/* apply now up pg button  */}
-                <Link to={"/Addmition/Enroll"}>
+                {/* <Link to={"/Addmition/Enroll"}>
                   <button className="relative inline-flex h-10 overflow-hidden rounded-full p-0.5 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                     <span className="absolute  inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                     <span className=" font-BBH inline-flex h-full w-89 cursor-pointer items-center justify-center rounded-full shadow-blue-950 shadow-lg bg-emerald-500/80 hover:bg-gray-900 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
                       Apply Now UG / PG {">"}
                     </span>
                   </button>
-                </Link>
+                </Link> */}
               </div>
 
 
--- src/Components/page_comp/home/Hospitl_dashcomp.jsx
+++ src/Components/page_comp/home/Hospitl_dashcomp.jsx
@@ -1,35 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Hospitl_dashcomp() {
+  
+  const Item = ["🧭 Emergency" , "💉 Surgery", " 💅 Long-term care" ,"👩‍👧‍👦 Family treatment" ,"💜 Critical cases"];
+  const [useItem , setUseItem] = useState(null)
+  const [active , setActive] =  useState("")
+
+  switch (useItem) {
+    case 0:
+      console.log("zero");
+      break;
+  
+      case 1:
+        console.log("one");
+        break;
+      
+      case 2:
+        console.log("two");
+        break;
+      
+      case 3:
+        console.log("three");
+        break;
+      
+      case 4:
+        console.log("four");
+        break;
+        
+        
+
+    default:
+      break;
+  }
+
+
+  
+  
   return (
     <>
         <div className='w-full border bg-black flex'>
-            <div className='bg-gray-500 w-[30%] md:w-[15%] h-full p-2'>
+
+          <div className='bg-gray-500  w-[30%] md:w-[25%] h-full p-2'>
                 <div className='border h-full w-full not-md:text-[14px] gap-3  flex flex-col justify-center items-start '>
-                    
-                    <div className='bg-white/20 w-full p-1 font-alluser font-extrabold rounded-[5px] flex pl-2 cursor-pointer hover:bg-sky-300/50'>
-                    🧭 Emergency
-                    </div>
-                    
-                    <div className='bg-white/20 w-full p-1 font-alluser font-extrabold rounded-[5px] flex pl-2 cursor-pointer hover:bg-sky-300/50'>
-                    💉 Surgery
-                    </div>
-                    
-                    <div className='bg-white/20 w-full p-1 font-alluser font-extrabold rounded-[5px] flex pl-2 cursor-pointer hover:bg-sky-300/50'>
-                    💅 Long-term care
-                    </div>
-                    
-                    <div className='bg-white/20 w-full p-1 font-alluser font-extrabold rounded-[5px] flex pl-2 cursor-pointer hover:bg-sky-300/50'>
-                    👩‍👧‍👦 Family treatment
-                    </div>
-                    
-                    <div className='bg-white/20 w-full p-1 font-alluser font-extrabold rounded-[5px] flex pl-2 cursor-pointer hover:bg-sky-300/50'>
-                    💜 Critical cases
-                    </div>
+                    {Item.map((element,index)=>{ 
+                        return <div
+                          onClick={()=>{
+                            console.log(index);
+                            setUseItem(index)
+                            
+                          }}
+                          className={`
+                          bg-white/20 w-full p-1 font-alluser font-extrabold rounded-[5px] flex pl-2 cursor-pointer hover:bg-sky-300/50`}>
+                        {element}
+                        </div>
+                    })}
                 </div>
             </div>
 
-            <div className='bg-amber-950 w-[70%] md:w-[85%] h-full'>fyff</div>
+
+
+            <div className='bg-amber-950 w-[70%] md:w-[85%] h-full'>
+              {
+
+              }
+            </div>
         </div>
     </>
   )
