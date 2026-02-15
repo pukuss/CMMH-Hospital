8de0d150a2d7b2f28c41eabc6e5b4c2ddd148ede
prashanta mondal <prashantaa@gmail.com>
2026-02-15 13:30:22 +0530

    0.0.5v --Configure send data of a addmition user

--- src/appwrite/addmition_conf.js
+++ src/appwrite/addmition_conf.js
@@ -1,36 +1,44 @@
 import conf from '../conf/conf'
-import { Client , Databases,ID } from 'appwrite'
+import { Client, Databases, ID } from 'appwrite'
 
-
-
-export class FromDataService{
+export class FromDataService {
     client = new Client();
     databases;
 
-    constructor(){
+    constructor() {
         this.client
-        .setEndpoint(conf.appwrite_Url)
-        .setProject(conf.appwrite_ProjectId);
+            .setEndpoint(conf.appwrite_Url)
+            .setProject(conf.appwrite_ProjectId);
 
         this.databases = new Databases(this.client);
     }
 
-    async SEND_ADMI_DATA({ User_Name,Father_Name,Addhar_UID,HSP_Name,Contact_Number, nationility,category,gender,neet_UID, course}){
+    async SEND_ADMI_DATA({
+        User_Name,
+        Father_Name,
+        Addhar_UID,
+        HSP_Name,
+        Contact_Number,
+        nationility,
+        category,
+        gender,
+        neet_UID,
+        course
+    }) {
         try {
-            const res = await this.databases.createDocument(conf.appwrite_DetabaseId,conf.appwrite_CollectionId,ID.unique(),
-        { User_Name,Father_Name,Addhar_UID,HSP_Name,Contact_Number, nationility,category,gender,neet_UID, course});
+            const res = await this.databases.createDocument(
+                conf.appwrite_DatabaseId,      // ✅ ঠিক করা
+                conf.appwrite_CollectionId,
+                ID.unique(),
+                { User_Name, Father_Name, Addhar_UID, HSP_Name, Contact_Number, nationility, category, gender, neet_UID, course }
+            );
             return res;
-        } 
-        catch (error) {
-            console.log("SEND ADDMITION DATA ERROR", error);
+        } catch (error) {
+            console.log("SEND ADMISSION DATA ERROR", error);
             throw error;
         }
     }
-
-
 }
 
-
-
-const useFromdata = new FromDataService()
-export default useFromdata
+const useFromdata = new FromDataService();
+export default useFromdata;
